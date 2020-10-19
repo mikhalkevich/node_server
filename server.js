6726a86f0d9b8afd9d0fedf993baee31c932bac3
Alexandr <mikhalkevich@ya.ru>
2020-10-19 12:55:20 +0300

    res.write

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var http = require('http');
 http.createServer(function(req, res){
  res.writeHead(200, {'content-type': 'text/plain'});
- res.write('<h1>');
- res.write('test');
- res.write('</h1>');
+ res.write('<h1>test</h1>');
  res.write('Hello ');
  res.end('world!');
 }).listen(8128); 
